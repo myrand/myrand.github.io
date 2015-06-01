9ab94078f0550e32432a55947c5249ac4d2e4342
mircea <mircea_and@yahoo.com>
2015-06-02 00:28:19 +0300

    my github page style

---
+++ assets/jscripts/myScript.js
@@ -0,0 +1,8 @@
+/*
+$(window).load(function() {
+  $("body").removeClass("preload");
+});
+
+body.classList.remove("preload");
+*/
+document.getElementById("remove-class").classList.remove("preload");
